80d824c841690e21d6debbf2f821c69400fc52e0
Ronkugo <Ronkugo@gmail.com>
2018-03-29 23:24:04 -0500

    Init

---
+++ javascript.js
@@ -0,0 +1,42 @@
+// Application Definition || Controllers & Routing
+// =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
+// =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
+console.log("The-Boyz-Training || Application Loaded");
+console.log("=====================================================");
+var myName = "Robert";
+console.log("Hey its " + myName);
+
+
+
+var part1 = "Get on my level";
+var part2 = "boy";
+
+
+document.getElementById('learn1').innerHTML = "Hey Ron! Time to get started";
+document.getElementById('learn2').innerHTML = "Make the line 4 read:";
+document.getElementById('learn3').innerHTML = "\"Get on my level, boy! \" using variables";
+document.getElementById('learn4').innerHTML = " ";
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
