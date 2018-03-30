8ab703e8547d8bd74f462d2ffe47fc463864ce65
Ronkugo <Ronkugo@gmail.com>
2018-03-29 23:50:40 -0500

    Line 4 Complete

--- javascript.js
+++ javascript.js
@@ -15,7 +15,7 @@ var part2 = "boy";
 document.getElementById('learn1').innerHTML = "Hey Ron! Time to get started";
 document.getElementById('learn2').innerHTML = "Make the line 4 read:";
 document.getElementById('learn3').innerHTML = "\"Get on my level, boy! \" using variables";
-document.getElementById('learn4').innerHTML = " ";
+document.getElementById('learn4').innerHTML = part1 + ", " + part2 + "!";
 
 
 
